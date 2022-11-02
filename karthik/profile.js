38caeb6459cd68ee2bd140d4c4826031aeb4d6ce
Karthik1758 <116056246+Karthik1758@users.noreply.github.com>
2022-11-02 17:59:38 +0530

    Add files via upload

---
+++ karthik/profile.js
@@ -0,0 +1,18 @@
+$("document").ready(()=>{
+    $.ajax({
+        url:'https://randomuser.me/api/',
+        dataType:'json',
+        success:function(data){
+            $("#FName").html(data.results[0].name.first)
+            $("#LName").html(data.results[0].name.last)
+            $("#Email").html(data.results[0].email)
+            $("#Gender").html(data.results[0].gender)
+            $("#DOB").html(data.results[0].dob.date)
+            $("#PhNo").html(data.results[0].phone)
+            $("#Cell").html(data.results[0].cell)
+            $("#Nationality").html(data.results[0].nat)
+            $("#ID").html(data.results[0].id.name+" "+data.results[0].id.value)
+
+        }
+    });
+});
